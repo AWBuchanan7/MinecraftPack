62dbc723f35a55d72ea82c5eb1bc3cf89ab938e7
Scott Lamb <sentientscythe@gmail.com>
2019-05-07 16:39:24 -0600

    Update ServerStart.bat

--- ServerStart.bat
+++ ServerStart.bat
@@ -3,7 +3,7 @@
 call settings.bat
 
 :start_server
-echo Starting MystTech Server...
+echo Starting Server...
 java -server -Xms%MIN_RAM% -Xmx%MAX_RAM% %JAVA_PARAMETERS% -jar %SERVER_JAR% nogui
 exit /B
 
